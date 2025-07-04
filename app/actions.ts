f3c718fd45b278845ed6bb7bd6972e2c457230cb
Bruno Branco <brunocbranco16@gmail.com>
2025-07-03 22:29:08 -0300

    fix build

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,5 @@
 "use server";
 import { createClient } from "@/utils/supabase/server";
-import { FormSchema } from "./components/lead-form";
 import statesJson from "../lib/estado-por-ddd.json";
 
 function findStateByDDD(ddd: string) {
@@ -8,20 +7,20 @@ function findStateByDDD(ddd: string) {
   const state = statesJson[ddd];
   return state || null;
 }
-export async function createLead(data: FormSchema) {
-  const supabase = await createClient();
-  const cleanedPhone = data.phone.replace(/\D/g, "").trim();
-  const ddd = cleanedPhone.slice(0, 2);
-
-  let state = findStateByDDD(ddd);
-  console.log(state);
-
-  const newLead = await supabase.from("leads").upsert({
-    name: data.name,
-    email: data.email,
-    phone: cleanedPhone,
-    state: state,
-    origin: data.origin,
-  });
-  return newLead;
-}
+// export async function createLead(data: FormSchema) {
+//   const supabase = await createClient();
+//   const cleanedPhone = data.phone.replace(/\D/g, "").trim();
+//   const ddd = cleanedPhone.slice(0, 2);
+//
+//   let state = findStateByDDD(ddd);
+//   console.log(state);
+//
+//   const newLead = await supabase.from("leads").upsert({
+//     name: data.name,
+//     email: data.email,
+//     phone: cleanedPhone,
+//     state: state,
+//     origin: data.origin,
+//   });
+//   return newLead;
+// }
